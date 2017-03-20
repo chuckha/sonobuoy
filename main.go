46ef4ac33531d9f28d91b58179d6a01e48350d75
Timothy St. Clair <timothysc@gmail.com>
2017-03-20 08:54:07 -0500

    Fills in base level concepts needed to run sonobuoy.
    Signed-off-by: Timothy St. Clair <timothysc@gmail.com>

--- main.go
+++ main.go
@@ -17,9 +17,78 @@ limitations under the License.
 package main
 
 import (
-	"fmt"
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/golang/glog"
+	"github.com/spf13/viper"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
+// setup a signal hander to gracefully exit
+func sigHandler() <-chan struct{} {
+	stop := make(chan struct{})
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c,
+			syscall.SIGINT,  // Ctrl+C
+			syscall.SIGTERM, // Termination Request
+			syscall.SIGSEGV, // FullDerp
+			syscall.SIGABRT, // Abnormal termination
+			syscall.SIGILL,  // illegal instruction
+			syscall.SIGFPE)  // floating point - this is why we can't have nice things
+		sig := <-c
+		glog.Warningf("Signal (%v) Detected, Shutting Down", sig)
+		close(stop)
+	}()
+	return stop
+}
+
+// loadConfig will parse input + config file and return a clientset
+func loadConfig() kubernetes.Interface {
+	var config *rest.Config
+	var err error
+
+	flag.Parse()
+
+	// leverages a file|(ConfigMap)
+	// to be located at /etc/sonobuoy/config
+	viper.SetConfigType("json")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("/etc/sonobuoy/")
+	viper.AddConfigPath(".")
+	viper.SetDefault("kubeconfig", "")
+	if err = viper.ReadInConfig(); err != nil {
+		panic(err.Error())
+	}
+
+	// allows for running both in & out of cluster
+	kubeconfig := viper.GetString("kubeconfig")
+	if len(kubeconfig) > 0 {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+	} else {
+		config, err = rest.InClusterConfig()
+	}
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// creates the clientset from kubeconfig
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	return clientset
+}
+
+// main entry point of the program
 func main() {
-	fmt.Printf("Hello World\n")
+
+	//clientset := loadConfig()
+
 }
